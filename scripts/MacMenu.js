bf5995487211095e0527975802e17b1cd8502b44
DavidBakalov21 <davidbakalov33@gmail.com>
2023-10-24 18:10:00 +0300

    prices

--- scripts/MacMenu.js
+++ scripts/MacMenu.js
@@ -1,22 +1,20 @@
 let defaultOrderCounts = {
-    "Royal Cheese": 0,
-    "Cheeseburger": 0,
-    "Hamburger": 0,
-    "McChicken": 0,
-    "Double Royal Cheese": 0,
-    "Chicken Roll": 0,
-    "Medium fries": 0,
-    "Large fries": 0,
-    "Chicken McNuggets": 0
-  };
+    "Royal Cheese": { count: 0, price: 6.99 },
+    "Cheeseburger": { count: 0, price: 8.49 },
+    "Hamburger": { count: 0, price: 4.99 },
+    "McChicken": { count: 0, price: 4.49 },
+    "Double Royal Cheese": { count: 0, price: 9.99 },
+    "Chicken Roll": { count: 0, price: 5.79 },
+    "Medium fries": { count: 0, price: 5.19 },
+    "Large fries": { count: 0, price: 6.79 },
+    "Chicken McNuggets": { count: 0, price: 5.99 }
+};
   let orderCounts = JSON.parse(localStorage.getItem('orderCounts')) || defaultOrderCounts;
   function addToOrder(food) {
-      orderCounts[food]++;
-
-      console.log(`${food} has been ordered ${orderCounts[food]} times.`);
-      localStorage.setItem('orderCounts', JSON.stringify(orderCounts));
-      showOverlay(food);
-  }
+    orderCounts[food].count++;
+    localStorage.setItem('orderCounts', JSON.stringify(orderCounts));
+    showOverlay(food);
+}
 
   function showOverlay(text) {
     const savedData = JSON.parse(localStorage.getItem('orderCounts'));
@@ -24,7 +22,7 @@ let defaultOrderCounts = {
     console.log(count);
     document.getElementById('overlay').classList.remove('hidden');
     document.getElementById("ChoiceText").textContent="You ordered "+text;
-    document.getElementById("amount").textContent="You already have "+count;
+    document.getElementById("amount").textContent="You already have "+count.count;
 }
 
 function closeOverlay() {
--- scripts/cart.js
+++ scripts/cart.js
@@ -1,28 +1,60 @@
+let defaultOrderCounts = {
+  "Royal Cheese": { count: 0, price: 6.99 },
+  "Cheeseburger": { count: 0, price: 8.49 },
+  "Hamburger": { count: 0, price: 4.99 },
+  "McChicken": { count: 0, price: 4.49 },
+  "Double Royal Cheese": { count: 0, price: 9.99 },
+  "Chicken Roll": { count: 0, price: 5.79 },
+  "Medium fries": { count: 0, price: 5.19 },
+  "Large fries": { count: 0, price: 6.79 },
+  "Chicken McNuggets": { count: 0, price: 5.99 }
+};
+let orderCounts = JSON.parse(localStorage.getItem('orderCounts')) || defaultOrderCounts;
 
-let orderCounts = {};
-
-// Load the data from localStorage when the page loads
 document.addEventListener('DOMContentLoaded', function() {
-  const savedData = localStorage.getItem('orderCounts');
-  
-  if (savedData) {
-    orderCounts = JSON.parse(savedData);
-    // Optionally: Update the UI based on the retrieved data
-    displayOrderCounts(orderCounts);
-  }
+  displayOrderCounts(orderCounts);
 });
 
+function clearCart() {
+  document.getElementById('overlay').classList.remove('hidden');
+}
+
+function closeOverlay() {
+  document.getElementById('overlay').classList.add('hidden');
+}
+
+function deleteOrder() {
+  localStorage.removeItem('orderCounts');
+  orderCounts = { ...defaultOrderCounts };
+  displayOrderCounts(orderCounts);
+}
+
+function confirm() {
+  document.getElementById('overlay').classList.remove('hidden');
+}
 
 function displayOrderCounts(orders) {
-    Object.entries(orders).forEach(([key, value]) => {
-        console.log(`Food: ${key}, Count: ${value}`);
-    });
-  }
+  let container = document.getElementById("orderedItems");
+  container.innerHTML = "";
 
+  let total = 0;
 
-  function clear(){
-document.getElementById('overlay').classList.remove('hidden');
-  }
-  function closeOverlay() {
-    document.getElementById('overlay').classList.add('hidden');
+  Object.entries(orders).forEach(([key, item]) => {
+      if (item.count > 0) {
+          const itemElem = document.createElement("div");
+          itemElem.classList.add("ordered-item");
+
+          const priceForAllItems = item.count * item.price;
+          total += priceForAllItems;
+
+          itemElem.textContent = `Food: ${key}, Count: ${item.count}, Total Price: $${priceForAllItems.toFixed(2)}`;
+          container.appendChild(itemElem);
+      }
+  });
+
+  // Display total amount if required
+  const totalElem = document.createElement("div");
+  totalElem.classList.add("total-amount");
+  totalElem.textContent = `Total: $${total.toFixed(2)}`;
+  container.appendChild(totalElem);
 }
