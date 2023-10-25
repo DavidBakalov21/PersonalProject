21a8ca42c0ad65e59172c643c5d669ae2eec5358
DavidBakalov21 <davidbakalov33@gmail.com>
2023-10-25 20:14:20 +0300

    mostly done

--- scripts/MacMenu.js
+++ scripts/MacMenu.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+  const cards = document.querySelectorAll('.Card');
+  cards.forEach((card, index) => {
+      setTimeout(() => {
+          card.classList.add('animate-card');
+      }, 250 * index);
+  });
+}
 let defaultOrderCounts = {
     "Royal Cheese": { count: 0, price: 6.99 },
     "Cheeseburger": { count: 0, price: 8.49 },
@@ -7,7 +15,20 @@ let defaultOrderCounts = {
     "Chicken Roll": { count: 0, price: 5.79 },
     "Medium fries": { count: 0, price: 5.19 },
     "Large fries": { count: 0, price: 6.79 },
-    "Chicken McNuggets": { count: 0, price: 5.99 }
+    "Chicken McNuggets": { count: 0, price: 5.99 },
+    "Toast": { count: 0, price: 6.99 },
+    "Twister": { count: 0, price: 8.49 },
+    "Dips": { count: 0, price: 4.99 },
+    "Chefburger": { count: 0, price: 4.49 },
+    "Boxmaster": { count: 0, price: 9.99 },
+    "Bakcet": { count: 0, price: 5.79 },
+    "BBQ pizza": { count: 0, price: 6.99 },
+    "Extravaganzza": { count: 0, price: 8.49 },
+    "Greece pizza": { count: 0, price: 4.99 },
+    "Grill pizza": { count: 0, price: 4.49 },
+    "Karbonara": { count: 0, price: 9.99 },
+    "Marharyta": { count: 0, price: 5.79 }
+  
 };
   let orderCounts = JSON.parse(localStorage.getItem('orderCounts')) || defaultOrderCounts;
   function addToOrder(food) {
